4ec00861a8ba098b31e959f60dec2738893e82cb
Paco <paco.refractando@gmail.com>
2026-02-06 13:11:50 +0100

    Feat: New GatewayV3 is registering in Admin as intended.

--- deployment/fullCompileDeploy.bat
+++ deployment/fullCompileDeploy.bat
@@ -9,18 +9,24 @@ echo ======================================================
 
 ::echo Compilando Eureka Server...
 ::call docker-compose -f deployment/docker-compose.yml build --no-cache eureka-server
-::if %errorlevel% neq 0 (echo Error en Eureka Server && pause && exit /b)
+
+:: OPCIONES DE BUILD:
+::--no-cache
 
 echo Compilando Config Service...
-call docker-compose -f docker-compose.yml build --no-cache config-service|| (echo Error en Config Service && pause && exit /b)
-::if %errorlevel% neq 0 (echo Error en Config Service && pause && exit /b)
+::call docker-compose -f docker-compose.yml build config-service|| (echo Error en Config Service && pause && exit /b)
 
 echo Compilando Admin...
-call docker-compose -f docker-compose.yml build --no-cache admin-server|| (echo Error en Admin && pause && exit /b)
+::call docker-compose -f docker-compose.yml build  admin-server|| (echo Error en Admin && pause && exit /b)
 
-echo Compilando API Gateway...
-call docker-compose -f docker-compose.yml build --no-cache api-gateway|| (echo Error en API Gateway && pause && exit /b)
-::if %errorlevel% neq 0 (echo Error en API Gateway && pause && exit /b)
+::echo Compilando API Gateway...
+::call docker-compose -f docker-compose.yml build --no-cache api-gateway|| (echo Error en API Gateway && pause && exit /b)
+
+echo Compilando API Gateway V3...
+call docker-compose -f docker-compose.yml build  api-gateway-v3|| (echo Error en API Gateway && pause && exit /b)
+
+echo Compilando Auction Service...
+call docker-compose -f docker-compose.yml build --no-cache auction-service|| (echo Error en Auction Service && pause && exit /b)
 
 echo Levantando infraestructura completa...
 call docker-compose -f docker-compose.yml up -d
@@ -28,5 +34,7 @@ call docker-compose -f docker-compose.yml up -d
 echo ======================================================
 echo   Despliegue finalizado
 echo   Eureka: http://localhost:8761
+echo   Admin: http://localhost:9090/admin
+echo   API Gateway: http://localhost:8080
 echo ======================================================
 pause
